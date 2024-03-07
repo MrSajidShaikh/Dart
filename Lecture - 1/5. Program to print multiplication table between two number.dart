1b8878c6336d863e6f33c22bce5fdbce156dafb1
sajid shaikh <149478269+mrsajidshaikh@users.noreply.github.com>
2024-03-07 13:58:31 +0530

    Add files via upload

---
+++ Lecture - 1/5. Program to print multiplication table between two number.dart
@@ -0,0 +1,21 @@
+import 'dart:io' ;
+main()
+{
+  stdout.write("Enter the starting number : ");
+  String x = stdin.readLineSync()!;
+  int start = int.parse(x);
+
+  stdout.write("Enter the ending number : ");
+  String y = stdin.readLineSync()!;
+  int end = int.parse(y);
+
+  for(int i=start; i<=end; i++)
+  {
+    for(int j=1; j<=10; j++)
+    {
+      int a = i*j;
+
+      print("$i x $j = $a");
+    }
+  }
+}
